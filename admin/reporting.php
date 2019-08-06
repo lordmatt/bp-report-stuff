f53f4979cacbf736b15cedf26cfa5d657cab3f1e
Lord Matt <letters@lordmatt.co.uk>
2019-08-07 00:07:32 +0100

    Still chipping away

---
+++ admin/reporting.php
@@ -0,0 +1,17 @@
+<?php
+
+/** 
+ * The template for showing the reporting report in admin UI
+ */
+
+?>
+<h1><?php _e("Community reporting and feedback",'bp_report_stuff'); ?></h1>
+
+<?php do_action('bp_report_stuff_admin_top'); ?>
+
+
+
+
+
+
+<?php do_action('bp_report_stuff_admin_footer'); ?>
